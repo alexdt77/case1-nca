5c2babf5bc9124bc76c4d41814110c05a5a52d85
alexdt77 <alexdimk245@gmail.com>
2025-09-14 15:39:10 +0200

    Create versions.tf

---
+++ infra/envs/dev/versions.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws    = { source = "hashicorp/aws", version = "~> 5.0" }
+    random = { source = "hashicorp/random", version = "~> 3.6" }
+  }
+  # backend "s3" {}  # later toevoegen (S3 + DynamoDB lock)
+}
