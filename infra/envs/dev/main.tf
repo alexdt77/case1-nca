71dd4ef954b4887baeeacc566ab589e4f39c3224
alexdt77 <alexdimk245@gmail.com>
2025-09-14 15:44:27 +0200

    Create main.tf

---
+++ infra/envs/dev/main.tf
@@ -0,0 +1,7 @@
+resource "random_id" "suffix" { byte_length = 3 }
+
+resource "aws_s3_bucket" "tf_test" {
+  bucket        = "${var.project}-${var.env}-tf-test-${random_id.suffix.hex}"
+  force_destroy = true
+  tags = { Project = var.project, Env = var.env }
+}
