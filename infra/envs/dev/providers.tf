6d4f0c7b35dc346af70994e808fad20b25d724eb
alexdt77 <alexdimk245@gmail.com>
2025-09-14 18:16:54 +0200

    Update providers.tf

--- infra/envs/dev/providers.tf
+++ infra/envs/dev/providers.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = { source = "hashicorp/aws",    version = "~> 5.0" }
+    random = { source = "hashicorp/random", version = "~> 3.6" }
+  }
+}
+
 provider "aws" {
-  region = "eu-central-1"  # Frankfurt
+  region = "eu-central-1"
 }
