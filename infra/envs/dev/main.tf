62771921d9346a19d01f0b2eb0c68a9a5420fb1d
alexdt77 <alexdimk245@gmail.com>
2025-09-14 18:17:22 +0200

    Update main.tf

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -1,8 +1,12 @@
-# trigger pipeline
-resource "random_id" "suffix" { byte_length = 3 }
+resource "random_id" "suffix" {
+  byte_length = 3
+}
 
 resource "aws_s3_bucket" "tf_test" {
   bucket        = "${var.project}-${var.env}-tf-test-${random_id.suffix.hex}"
   force_destroy = true
-  tags = { Project = var.project, Env = var.env }
+  tags = {
+    Project = var.project
+    Env     = var.env
+  }
 }
