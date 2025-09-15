dd8ce49c0e5dd3adcb86e60a7fe8f84db6a74f10
alexdt77 <alexdimk245@gmail.com>
2025-09-15 16:27:03 +0200

    Update variables.tf

--- infra/envs/dev/variables.tf
+++ infra/envs/dev/variables.tf
@@ -7,3 +7,12 @@ variable "env" {
   type    = string
   default = "dev"
 }
+
+variable "aws_region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "cidr_hub"  { type = string  default = "10.0.0.0/24" }
+variable "cidr_app"  { type = string  default = "10.0.1.0/24" }
+variable "cidr_data" { type = string  default = "10.0.2.0/24" }
