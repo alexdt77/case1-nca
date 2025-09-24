37ad20ad74d2c8ba49cadb8968a1af100bfee152
alexdt77 <alexdimk245@gmail.com>
2025-09-24 15:39:08 +0200

    Update main.tf

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -1,10 +1,10 @@
 output "region" { value = var.aws_region }
 
+
 data "aws_secretsmanager_secret" "db_pass" {
   name = "caselnca/db-pass"
 }
 
-resource "aws_secretsmanager_secret_version" "db_pass" {
-  secret_id     = data.aws_secretsmanager_secret.db_pass.id
-  secret_string = var.db_master_password
+data "aws_secretsmanager_secret_version" "db_pass" {
+  secret_id = data.aws_secretsmanager_secret.db_pass.id
 }
