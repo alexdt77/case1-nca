b644d25dd43f87fc2ebe57ebc1ee84a2ccf93d22
alexdt77 <alexdimk245@gmail.com>
2025-09-24 15:23:37 +0200

    Update main.tf

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -8,9 +8,3 @@ resource "aws_secretsmanager_secret_version" "db_pass" {
   secret_id     = data.aws_secretsmanager_secret.db_pass.id
   secret_string = var.db_master_password
 }
-
-resource "aws_secretsmanager_secret_version" "db_pass" {
-  secret_id     = aws_secretsmanager_secret.db_pass.id
-  secret_string = var.db_master_password
-}
-
